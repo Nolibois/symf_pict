205b6c21ca5400eda5c30e8736e3f8bf4b164f18
Olivier Nolibois <olivier.nolibois@gmail.com>
2021-09-25 21:20:15 +0200

    Dependence injection

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -2,15 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\Pin;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PinsController extends AbstractController
 {
+
     #[Route('/')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
+        $pin = new Pin;
+        $pin->setTitle('Mon 3ème titre');
+        $pin->setDescription('Ceci est une longue 3ème description...');
+
+        $em->persist($pin);
+        $em->flush();
+
+        dump($pin);
+
         return $this->render('pins/index.html.twig');
     }
 
