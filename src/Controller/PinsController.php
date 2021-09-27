578a63f871eb7e181bfdddb8a5bf048687ccf827
Olivier Nolibois <olivier.nolibois@gmail.com>
2021-09-27 22:03:03 +0200

    twig conditions

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -8,9 +8,6 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PinsController extends AbstractController
@@ -19,27 +16,22 @@ class PinsController extends AbstractController
     #[Route('/', name: 'app_home', methods: "GET")]
     public function index(PinRepository $repo): Response
     {
-
         return $this->render('pins/index.html.twig', ['pins' => $repo->findAll()]);
     }
 
     #[Route('/pins/create', name: 'app_pin_create', methods: ["GET", "POST"])]
     public function create(Request $request, EntityManagerInterface $em): Response
     {
-        $form = $this->createFormBuilder()
-            ->add('title', TextType::class, ['attr' => ['autofocus' => true],])
-            ->add('description', TextareaType::class, ['attr' => ['rows' => 10, 'cols' => 60, 'placeholder' => 'Ecrivez votre description ici']])
-            ->add('submit', SubmitType::class, ['label' => 'Envoyer'])
+        $pin = new Pin;
+
+        $form = $this->createFormBuilder($pin)
+            ->add('title', null, ['attr' => ['autofocus' => true],])
+            ->add('description', null, ['attr' => ['rows' => 10, 'cols' => 60, 'placeholder' => 'Ecrivez votre description ici']])
             ->getForm();
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $data = $form->getData();
-
-            $pin = new Pin;
-            $pin->setTitle($data['title']);
-            $pin->setDescription($data['description']);
 
             $em->persist($pin);
             $em->flush();
