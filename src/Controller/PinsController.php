0a64397315c835f9de0ca4a5781571d464c963c8
Olivier Nolibois <olivier.nolibois@gmail.com>
2021-09-27 19:18:40 +0200

    options form builder

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -4,42 +4,49 @@ namespace App\Controller;
 
 use App\Entity\Pin;
 use App\Repository\PinRepository;
-use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PinsController extends AbstractController
 {
 
-    #[Route('/', name: 'app_home')]
-    public function index(PinRepository $repo): Response //ou index(EntityManagerInterface $em)
+    #[Route('/', name: 'app_home', methods: "GET")]
+    public function index(PinRepository $repo): Response
     {
-        /* $repo = $em->getRepository(Pin::class);
-        $pins = $repo->findAll(); */
 
-        return $this->render('pins/index.html.twig', ['pins' => $repo->findAll()]); // compact('pins') ou = ["pins" => $pins]
+        return $this->render('pins/index.html.twig', ['pins' => $repo->findAll()]);
     }
 
     #[Route('/pins/create', name: 'app_pin_create', methods: ["GET", "POST"])]
     public function create(Request $request, EntityManagerInterface $em): Response
     {
-        if ($request->isMethod("POST")) {
-            $data = $request->request->all();
+        $form = $this->createFormBuilder()
+            ->add('title', TextType::class, ['attr' => ['autofocus' => true],])
+            ->add('description', TextareaType::class, ['attr' => ['rows' => 10, 'cols' => 60, 'placeholder' => 'Ecrivez votre description ici']])
+            ->add('submit', SubmitType::class, ['label' => 'Envoyer'])
+            ->getForm();
 
-            if ($this->isCsrfTokenValid('pins_create', $data['_token'])) {
-                $pin = new Pin;
-                $pin->setTitle($data['title']);
-                $pin->setDescription($data['description']);
+        $form->handleRequest($request);
 
-                $em->persist($pin);
-                $em->flush();
-            }
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $pin = new Pin;
+            $pin->setTitle($data['title']);
+            $pin->setDescription($data['description']);
+
+            $em->persist($pin);
+            $em->flush();
 
             return $this->redirectToRoute('app_home');
         }
 
-        return $this->render('pins/create.html.twig');
+        return $this->render('pins/create.html.twig', ['createForm' => $form->createView()]);
     }
 }
