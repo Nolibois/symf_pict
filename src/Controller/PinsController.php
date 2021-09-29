08d6a66f772aa03c4712830c76cdfa3eff015cf1
Olivier Nolibois <olivier.nolibois@gmail.com>
2021-09-29 18:45:43 +0200

    Path to id

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Flex\Path;
 
 class PinsController extends AbstractController
 {
@@ -17,6 +18,13 @@ class PinsController extends AbstractController
     public function index(PinRepository $repo): Response
     {
         return $this->render('pins/index.html.twig', ['pins' => $repo->findAll()]);
+    }
+
+
+    #[Route('/pins/{id<[0-9]+>}', name: "app_pins_show")]
+    public function showPin(Pin $pin): Response
+    {
+        return $this->render('pins/showPin.html.twig', compact('pin'));
     }
 
     #[Route('/pins/create', name: 'app_pin_create', methods: ["GET", "POST"])]
@@ -36,7 +44,7 @@ class PinsController extends AbstractController
             $em->persist($pin);
             $em->flush();
 
-            return $this->redirectToRoute('app_home');
+            return $this->redirectToRoute('app_pins_show', ["id" => $pin->getId()]);
         }
 
         return $this->render('pins/create.html.twig', ['createForm' => $form->createView()]);
